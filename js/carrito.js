bd9ed5188a63e90f457e75f518b7678eb58ee9af
Andreaco <andreacardosool18@gmail.com>
2026-06-02 15:48:58 -0600

    yaaaa me encantó

--- js/carrito.js
+++ js/carrito.js
@@ -3,26 +3,53 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function ActualizarContadorMenu() {
+
     const badge = document.getElementById('contador-carrito');
-    if (!badge) return; 
+
+    if (!badge) return;
 
     let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
-    let totalProductos = carrito.reduce((total, item) => total + item.cantidad, 0);
-    
+
+    let totalProductos = carrito.reduce(
+        (total, item) => total + item.cantidad,
+        0
+    );
+
     badge.textContent = totalProductos;
 }
 
+function MostrarNotificacion(mensaje){
+
+    document.getElementById('toastMensaje').textContent = mensaje;
+
+    const toast = new bootstrap.Toast(
+        document.getElementById('toastCarrito')
+    );
+
+    toast.show();
+}
+
 function AgregarAlCarritoGlobal(id, nombre, precio, imagen) {
+
     let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
-    
+
     const existe = carrito.find(p => p.id === id);
+
     if (existe) {
         existe.cantidad += 1;
     } else {
-        carrito.push({ id, nombre, precio, imagen, cantidad: 1 });
+        carrito.push({
+            id,
+            nombre,
+            precio,
+            imagen,
+            cantidad: 1
+        });
     }
-    
+
     localStorage.setItem('carrito', JSON.stringify(carrito));
+
     ActualizarContadorMenu();
-    alert(`¡${nombre} se agregó al carrito con éxito!`);
+
+    MostrarNotificacion(`🛒 ${nombre} agregado al carrito`);
 }
